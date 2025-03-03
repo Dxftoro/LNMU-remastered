69645062509cc1a1a7ddf0b4a84507e2ba94ce76
Dxftoro <96373847+Dxftoro@users.noreply.github.com>
2025-03-03 20:03:02 +0200

    Delete LNMU_Client directory

--- LNMU_Client/LNMU_Client/DebugView.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<string>
-class DebugView {
-public:
-	std::string error[2] = { "Can't load WSA library!", "Server connection failed!" };
-	std::string result[1] = { "Client connection succesful!" };
-	std::string msgs[1] = { "Succesfuly connected to the server!" };
-};
